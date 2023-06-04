4994e19049b17a36813831acb19b2570c97b4ea6
Maxim Belov <belov1988@gmail.com>
2023-06-04 18:07:48 +0300

    try sdk 15

--- runAndroidSample.sh
+++ runAndroidSample.sh
@@ -1,11 +1,10 @@
 clear
-jenv global 1.8
-export ANDROID_SDK_ROOT=~/Library/Android/sdk
-export PATH=${PATH}:${ANDROID_HOME}/tools
-export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+#jenv global 1.8
+#export ANDROID_SDK_ROOT=~/Library/Android/sdk
+#export PATH=${PATH}:${ANDROID_HOME}/tools
+#export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 cd Example
-cordova plugin remove ../intercom-plugin
-cordova plugin add ../intercom-plugin
-cordova platform remove android
-cordova platform add android@10.1.1 --save
+rm -rf {platforms,plugins}
+npm i
+cordova platform add android --save
 cordova run android --stacktrace
--- runiOSSample.sh
+++ runiOSSample.sh
@@ -1,7 +1,6 @@
 clear
 cd Example
-cordova plugin remove ../intercom-plugin
-cordova plugin add ../intercom-plugin
-cordova platform remove ios
+rm -rf {platforms,plugins}
+npm i
 cordova platform add ios --save
 cordova run ios --stacktrace
